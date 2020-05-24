cef8757e57c88b1b3e5f42769092219a7f9604ba
kijung <rlwjd4177@naver.com>
2020-05-24 17:26:18 +0900

    question home 완료

--- question/views.py
+++ question/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Question
+from django.utils import timezone
+
+def home(request):
+    questions = Question.objects.all()
+    return render(request,'home.html',{'questions': questions})
+
+def question(request,question_id):
+    questions = get_object_or_404(Question,pk = question_id)
+    return render(request,'question.html',{'question':questions})
+
 
 # Create your views here.
+
