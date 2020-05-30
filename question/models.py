c119580ce178cbd6f62e4b22091b774e817ef934
kijung <rlwjd4177@naver.com>
2020-05-30 17:53:11 +0900

    question작업완료

--- question/models.py
+++ question/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+# from ..answer.models import Answer
+
 # Create your models here.
 class Question(models.Model):
     title = models.CharField(max_length=200)
-    pup_date = models.DateTimeField()
+    pub_date = models.DateTimeField()
     body = models.TextField()
-
-
+    professor_name = models.CharField(max_length=200,null=True)
+    subject_name = models.CharField(max_length=200,null=True)
+    # answer = models.ForeignKey(Answer, on_delete= models.CASCADE)
