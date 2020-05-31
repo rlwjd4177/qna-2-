2eb5fab093b467f1b065491cf1b93b2c8af776c1
kijung <rlwjd4177@naver.com>
2020-05-31 16:04:10 +0900

    question u,d추가 및 css수정

--- question/urls.py
+++ question/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('<int:question_id>', views.question,name="question"),
     path('search',views.search,name="search"),
     path('new',views.new,name="new"),
+    path('delete/<int:question_id>',views.delete, name="delete"),
+    path('edit/<int:question_id>',views.edit, name="edit"),
 ]
 
